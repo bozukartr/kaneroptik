5b51d5f1ff73091e4c9a46d36f28134a98c8d3cc
Burak Göl <burak22286@gmail.com>
2026-07-20 15:14:10 +0300

    Add Firebase project configuration

---
+++ assets/firebase-config.js
@@ -0,0 +1,15 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBVZpMdP0X4UHfYXBF1nAtV7MtvIXuWFKc",
+  authDomain: "kaneroptik.firebaseapp.com",
+  projectId: "kaneroptik",
+  storageBucket: "kaneroptik.firebasestorage.app",
+  messagingSenderId: "774761005831",
+  appId: "1:774761005831:web:3961a797409f1c8cf37ba2",
+  measurementId: "G-7XXZ7EQZP3"
+};
+
+export const storeConfig = {
+  id: "kaner-optik",
+  authorizedEmail: "admin@kaneroptik.app",
+  autoLockMinutes: 15
+};
